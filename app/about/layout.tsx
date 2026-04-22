af9f0ce19cf66f0118f1b751901246556c92a005
aqueous-earth-catalog <aqueous.earth@gmail.com>
2026-04-22 13:39:41 -0400

    Update layout.tsx
    soften style

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,8 +1,10 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="max-w-7xl mx-auto px-4 sm:px-6 py-6 sm:py-12 min-h-screen bg-white">
-      <div className="max-w-4xl prose prose-sm lg:prose-lg prose-h1:font-bold prose-h1:tracking-tight prose-p:text-slate-700 prose-p:leading-relaxed">
-        {children}
+    <section className="min-h-screen bg-[#d3eff0]">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 py-6 sm:py-12">
+        <div className="max-w-4xl prose prose-sm sm:prose-base prose-headings:font-semibold prose-headings:tracking-normal prose-p:leading-7 prose-p:text-slate-700">
+          {children}
+        </div>
       </div>
     </section>
   );
