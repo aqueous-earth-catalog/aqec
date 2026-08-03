9ad7604dc7e583cdc2f5b12abe0d2ba2a0648af7
aqueous-earth-catalog <aqueous.earth@gmail.com>
2026-08-03 12:13:16 -0400

    Update mode-toggle.tsx
    dark mode button size fix on mobile view

--- components/theme/mode-toggle.tsx
+++ components/theme/mode-toggle.tsx
@@ -18,11 +18,15 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+<Button
+  variant="outline"
+  size="icon"
+  className="h-7 w-7 md:h-9 md:w-9"
+>
+  <Sun className="h-3.5 w-3.5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 md:h-[1.2rem] md:w-[1.2rem]" />
+  <Moon className="absolute h-3.5 w-3.5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 md:h-[1.2rem] md:w-[1.2rem]" />
+  <span className="sr-only">Toggle theme</span>
+</Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
